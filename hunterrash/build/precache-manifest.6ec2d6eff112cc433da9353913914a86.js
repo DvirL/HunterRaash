2ff7b0113505f7451c1bfceee92077cc442a5550
Unknown <dvir.laufer23@gmail.com>
2019-04-03 23:50:30 +0300

    adding build

---
+++ hunterrash/build/precache-manifest.6ec2d6eff112cc433da9353913914a86.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4ee02456f5669b8f5e71",
+    "url": "/static/js/main.9f822b61.chunk.js"
+  },
+  {
+    "revision": "54b241c47a9ca9c05a50",
+    "url": "/static/js/2.376178fe.chunk.js"
+  },
+  {
+    "revision": "4ee02456f5669b8f5e71",
+    "url": "/static/css/main.45ab0e60.chunk.css"
+  },
+  {
+    "revision": "798380d1287ee5e81289773f7a91aa15",
+    "url": "/index.html"
+  }
+];
