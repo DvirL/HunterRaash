a95be2e1cad5faeb3726648ac0dfdfde3d8c3e81
Unknown <dvir.laufer23@gmail.com>
2019-04-13 17:23:33 +0300

    refactor in Sound.jsx

--- src/components/Sound/index.jsx
+++ src/components/Sound/index.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './sound.css';
 
-export default class Sound extends React.Component {
-  static propTypes = {
-    image: PropTypes.string.isRequired,
-    url: PropTypes.string.isRequired,
-  };
-  
-  play = () => {
-    const a = new Audio(this.props.url);
+const Sound = ({ image, url }) => {
+  new Audio(url); // this is for first time download
+
+  const play = () => {
+    const a = new Audio(url);
     a.play();
   };
 
-  render() {
-    return (
-      <img className="sound" src={this.props.image} onClick={this.play} alt={this.props.image}/>
-    );
-  }
-}; 
+  return (
+    <img className="sound" src={image} onClick={play} alt={image} />
+  );
+};
+
+Sound.propTypes = {
+  image: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired,
+};
+
+export default Sound;
